eb9eb7abc22426a484293e1d9f176022ca81278e
Gustavo Etchepare <cgetchepare@gmail.com>
2025-05-30 11:08:49 -0300

    components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,14 @@
+import Menu from "@/components/Menu";
+import Footer from "@/components/Footer";
+
 const About = () => {
     return (
         <main>
+            <Menu />
             <h2>Sobre</h2>
+            <Footer />
         </main>
     )
-}
+};
 
 export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Menu from "@/components/Menu";
+import Footer from "@/components/Footer";
+
 const Home = () => {
-  return(
+  return (
     <main>
+      <Menu />
       <h2>Bem-vindo!</h2>
+      <Footer />
     </main>
   )
-}
+};
 
 export default Home;
 
