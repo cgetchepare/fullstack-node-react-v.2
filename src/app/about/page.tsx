0c02a3f9051015f3a1f7e4da0890d533cb155d55
Gustavo Etchepare <cgetchepare@gmail.com>
2025-05-30 10:43:55 -0300

    pages created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <main>
+            <h2>Sobre</h2>
+        </main>
+    )
+}
+
+export default About;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <main>
+            <h2>Contato</h2>
+        </main>
+    )
+}
+
+export default Contact;
